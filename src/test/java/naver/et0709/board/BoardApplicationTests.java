05c1416a0db3f277ed78f31f6516837638034fc1
king0jin <et0709@naver.com>
2024-11-13 09:43:54 +0900

    connect

---
+++ src/test/java/naver/et0709/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package naver.et0709.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
